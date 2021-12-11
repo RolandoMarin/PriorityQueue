6c57c92fbf18d2a1cc1efcc2de7552a847b92cac
Rolando Marin <RolandoGalvanM@outlook.com>
2021-12-11 00:01:27 -0800

    Add project files.

---
+++ Queue2/ERQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue2
+{
+    internal class ERQueue
+    {
+        private string _name;
+        private int _condition;
+        public ERQueue(string Name, int Condition) 
+        {
+        _name = Name;
+        _condition = Condition;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+       
+
+        public int Condition
+        {
+            get { return _condition; }
+        }
+
+        
+
+
+
+
+
+        public override string ToString()
+        {
+          return base.ToString();
+        }
+
+        
+
+
+
+
+
+    }
+}
---
+++ Queue2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue2
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Queue<ERQueue> myQueue = new Queue<ERQueue>();
+
+            string selectedChoice;
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write(Menu());
+
+                selectedChoice = Console.ReadLine();
+
+                //Add Patient
+                if (selectedChoice.ToUpper() == "A")
+                {
+
+                    Console.WriteLine("\nWho are we helping today?");
+
+                    string patient = Console.ReadLine();
+
+                    Console.WriteLine("Priority (1:highest priority – 5:lowest priority)");
+
+                    int mission = int.Parse(Console.ReadLine());
+
+                    myQueue.Enqueue(new ERQueue(patient, mission));
+
+                }
+
+                //shows us the name of patient and what they have
+                else if (selectedChoice.ToUpper() == "P")
+                {
+                   
+                    ERQueue next = myQueue.Dequeue();
+                  
+
+                    Console.WriteLine("\nYou are next " + next.Name);
+
+                    Console.WriteLine("I see you have a priotiry of " + next.Condition + " let me help you with that!");
+                    Console.WriteLine("All Done!");
+
+                    Console.WriteLine("Press enter for new patient");
+                    Console.ReadLine();
+                }
+
+                else if (selectedChoice.ToUpper() == "L")
+                {
+                    //string allNode = + myQueue.Dequeue();
+
+                    //while(myQueue.Dequeue() != null) 
+                    //{
+                    //    myQueue = myQueue.Dequeue();
+
+
+
+
+                }
+             else if (selectedChoice.ToUpper() == "Q")
+                 {
+                    break;
+            
+                 }
+                
+            }//quit
+           while (selectedChoice != "Q");
+            
+       
+
+
+            static string Menu()
+            {
+                return "(A)dd Patient\n" +
+                  "(P)roccess Current Patient \n" +
+                  "(L)ist all in Queue\n" +
+                  "(Q)uite\n\n" +
+                  "Choose Your option(A, P, L, Q): \n";
+            }
+
+         
+           
+
+
+
+        }
+    }
+}
